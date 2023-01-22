9f3ef69c06f538791d47a11f2bb22900ac2238c4
Sweet-Pumpkin <2013ssh3y95@naver.com>
2023-01-22 21:10:17 +0100

    Project testing

--- ts/main.ts
+++ ts/main.ts
@@ -1,2 +1,18 @@
 // CSS
 import '../css/style.css';
+// JSON
+fetch('./json/verb.json').then(res => {
+    return res.json();
+}).then(obj => {
+  console.log(obj);  
+})
+
+const root = document.getElementById('root');
+
+let template = `
+  <div class="header">
+    <h1>
+      <a href="#">ㄷㅇㅈ</a>
+    </h1>
+  </div>
+`
